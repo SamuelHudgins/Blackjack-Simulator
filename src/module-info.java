0ec5aede7a066246b33aeb2be1365a4d1f989da6
Robert Carswell <robert.carswell34@yahoo.com>
2023-11-15 09:47:04 -0700

    requirements for required for javafx @FXML and FXMLLoader

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,9 @@
  */
 module BlackJack {
 	requires java.desktop;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports FinalProject; //required for javafx @FXML
+    opens FinalProject; //required for javafx @FXML
 }
